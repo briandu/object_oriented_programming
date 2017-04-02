3dd92772ed6f9fdc77049ab5c4bb21f0dce7cc11
Brian Du <brian.bh.du@gmail.com>
2017-04-02 13:08:55 -0400

    Exercise 4
    - Changed attribute accessor to reader
    - Fixed ‘deliver’ method
    - Created ‘report’ method

--- exercise-4-paperboy/paperboy.rb
+++ exercise-4-paperboy/paperboy.rb
@@ -1,22 +1,37 @@
 class Paperboy
 
-  attr_reader :earnings
-  attr_accessor :name, :experience, :earnings
+  attr_reader :name, :experience, :earnings
 
-  def initialize(name, experience, earnings)
+  def initialize(name)
     @name = name
-    @experience = experience
-    @earnings = earnings
+    @experience = 0
+    @earnings = 0
   end
 
   def quota
-    experience = (@experience / 2) + 50
-    puts "#{name}'s new quota is #{experience}"
+    temp_exp = @experience / 2 + 50
   end
 
   def deliver(start_address, end_address)
-    @experience = (end_address - start_address) + @experience
+
+      if (end_address - start_address) <= quota
+        @earnings = (end_address - start_address) * 0.25 - 2.00
+
+      elsif (end_address - start_address) == quota
+        @earnings = (end_address - start_address) * 0.25
+
+      else
+        @earnings = (((end_address - start_address) - quota) * 0.50) + (quota * 0.25)
+
+      end
+
+      @experience = (end_address - start_address)
   end
+
+  def report
+    puts "#{@name} delivered #{@experience} papers and he has earned $#{@earnings}."
+  end
+
 end
 
 =begin
@@ -24,6 +39,12 @@ end
 # irb notes
 
 Brian = Paperboy.new("Brian",50,0)
+
+# Test situations
+
+  If delivery < quota:
+  Brian.deliver(100,170)
+
 
 Why does attr_reader have to be above attr_accessor?
 =end
