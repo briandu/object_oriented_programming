a486242770193f3757a186fab5877e7351eae839
Brian Du <brian.bh.du@gmail.com>
2017-04-02 11:35:47 -0400

    Exercise 4
    - Fixed ‘quota’ method equation
    - Created ‘deliver’ method
    - Added some notes

--- exercise-4-paperboy/paperboy.rb
+++ exercise-4-paperboy/paperboy.rb
@@ -1,7 +1,7 @@
 class Paperboy
 
   attr_reader :earnings
-  attr_accessor :name, :experience,
+  attr_accessor :name, :experience, :earnings
 
   def initialize(name, experience, earnings)
     @name = name
@@ -10,11 +10,20 @@ class Paperboy
   end
 
   def quota
-    @experience = (@experience / 2) + @experience
-    puts "#{name}'s new quota is #{@experience}'"
+    experience = (@experience / 2) + 50
+    puts "#{name}'s new quota is #{experience}"
   end
 
-  def deliver
-
+  def deliver(start_address, end_address)
+    @experience = (end_address - start_address) + @experience
   end
 end
+
+=begin
+
+# irb notes
+
+Brian = Paperboy.new("Brian",50,0)
+
+Why does attr_reader have to be above attr_accessor?
+=end
